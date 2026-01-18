ed07b6906b69de29abd8e0ba3e9edc8705921186
Lorant Marton <158153358+martonlori@users.noreply.github.com>
2026-01-18 12:11:01 +0100

    create chat.py, initial state of the chatbot

---
+++ scripts/chat.py
@@ -0,0 +1,87 @@
+import faiss
+import numpy as np
+import ollama
+
+INDEX_PATH = "data/vector_store/faiss.index"
+METADATA_PATH = "data/vector_store/metadata.npy"
+
+EMBED_MODEL = "nomic-embed-text"
+CHAT_MODEL = 'llama3'
+
+TOP_K = 4
+
+
+# Create embedding vector for user query with Ollama
+def embed_query(text):
+    response = ollama.embeddings(
+        model=EMBED_MODEL,
+        prompt=text
+    )
+    return np.array(response["embedding"], dtype="float32")
+
+# Load FAISS index and metadata to match the "most similar" vector to a chunk of text
+def load_vector_store():
+    index = faiss.read_index(INDEX_PATH)
+    metadata = np.load(METADATA_PATH, allow_pickle=True).tolist()
+    return index, metadata
+
+
+# Find the top-k "most similar" chunks to the user query, based on vector similarity
+def retrieve_chunks(query, index, metadata, k=TOP_K):
+    query_vector = embed_query(query).reshape(1, -1)
+    distances, indices = index.search(query_vector, k)
+
+    results = []
+    for i in indices[0]:
+        results.append(metadata[i])
+
+    return results
+
+
+# Build prompt
+def build_prompt(context_chunks, question):
+    context_text = "\n\n".join(chunk["text"] for chunk in context_chunks)
+    prompt = f"""
+You are a helpful SAP customer support assistant.
+Use ONLY the information in the context below to answer the question.
+If the answer is not in the context, say you don't know."
+
+Context:
+{context_text}
+
+Question:
+{question}
+
+Answer:
+"""
+    return prompt
+
+# Pass prompt to LLM
+def ask_llm(prompt):
+    response = ollama.chat(
+        model=CHAT_MODEL,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response["message"]["content"]
+
+
+def main():
+    print("Loading")
+    index, metadata = load_vector_store()
+
+    print("Chatbot ready, type a question (or 'exit').\n")
+
+    while True:
+        question = input("Your prompt: ")
+        if question.lower() == "exit":
+            break
+
+        retrieved_chunks = retrieve_chunks(question, index, metadata)
+        prompt = build_prompt(retrieved_chunks, question)
+        answer = ask_llm(prompt)
+
+        print("\nChatbot answer:\n", answer, "\n")
+
+
+if __name__ == "__main__":
+    main()
